02b296fbab89dbdf636ae734749bb32565b580b8
AashmannGarg <87020202+AashmannGarg@users.noreply.github.com>
2022-01-22 18:00:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,74 @@ function new_image(get_image){
         canvas.add(block_image_object);
     });
 }
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    var keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey && keyPressed=="80"){
+        console.log("p and Shift are pressed together");
+        block_image_width=block_image_width+10;
+        block_image_height=block_image_height+10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(e.shiftKey && keyPressed=="77"){
+        console.log("m and Shift are pressed together");
+        block_image_width=block_image_width-10;
+        block_image_height=block_image_height-10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if(keyPressed=="38"){
+        up();
+        console.log("up arrow is pressed");
+    }
+    if(keyPressed=="40"){
+        down();
+        console.log("down arrow is pressed");
+    }
+    if(keyPressed=="37"){
+        left();
+        console.log("left arrow is pressed");
+    }
+    if(keyPressed=="39"){
+        right();
+        console.log("right arrow is pressed");
+    }
+    if(keyPressed=="67"){
+        new_image("cloud.jpg");
+        console.log("c is pressed");
+    }
+    if(keyPressed=="68"){
+        new_image("dark_green.png");
+        console.log("d is pressed");
+    }
+    if(keyPressed=="71"){
+        new_image("ground.png");
+        console.log("g is pressed");
+    }
+    if(keyPressed=="76"){
+        new_image("light_green.png");
+        console.log("l is pressed");
+    }
+    if(keyPressed=="82"){
+        new_image("roof.jpg");
+        console.log("r is pressed");
+    }
+    if(keyPressed=="84"){
+        new_image("trunk.jpg");
+        console.log("t is pressed");
+    }
+    if(keyPressed=="85"){
+        new_image("unique.png");
+        console.log("u is pressed");
+    }
+    if(keyPressed=="87"){
+        new_image("wall.jpg");
+        console.log("w is pressed");
+    }
+    if(keyPressed=="89"){
+        new_image("yellow_wall.png");
+        console.log("y is pressed");
+    }
+}
